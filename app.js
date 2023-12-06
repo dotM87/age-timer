05ba3733dcf5dd042df9c59f2bc1bcde0ed9796e
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2023-12-05 21:55:11 -0400

    f

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const datePicker = document.createElement("input");
 datePicker.type = "date";
 // Save the selected date
 const selectedDate = datePicker.value;
-// Append the date picker to the document body or any other element
-document.body.appendChild(datePicker);
+
+document.getElementById("age-timer").innerHTML = datePicker;
