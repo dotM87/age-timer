cf69f458ea8c0915a2fe5220d3e4de5edd3cae05
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2023-12-05 21:43:31 -0400

    gg

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-// Aquí puedes comenzar a escribir tu código
 // Create a date picker element
 const datePicker = document.createElement("input");
 datePicker.type = "date";
-
+// Save the selected date
+const selectedDate = datePicker.value;
 // Append the date picker to the document body or any other element
 document.body.appendChild(datePicker);
