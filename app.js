29a5e68c142bc89dac82191003a1f49d404a637c
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2023-12-05 22:06:05 -0400

    wp?

--- app.js
+++
@@ -1,7 +0,0 @@
-// Create a date picker element
-const datePicker = document.createElement("input");
-datePicker.type = "date";
-// Save the selected date
-const selectedDate = datePicker.value;
-
-document.getElementById("age-timer").innerHTML = datePicker;
